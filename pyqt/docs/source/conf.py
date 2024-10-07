7ef9b9d1daa891de44582a6562378b4bb942d6b8
sajaborgholl <saja.borgholl@hotmail.com>
2024-10-07 16:23:32 +0300

    Add PyQt UI

---
+++ pyqt/docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+
+
+project = 'School Management System'
+copyright = '2024, Saja Borghol'
+author = 'Saja Borghol'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # If you're using Google-style docstrings
+    'sphinx.ext.viewcode',  # If you want to include links to the code
+]
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ pyqt/docs/source/index.rst
@@ -0,0 +1,16 @@
+Welcome to School Management System's documentation!
+====================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   school_management_system
+   operations
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
